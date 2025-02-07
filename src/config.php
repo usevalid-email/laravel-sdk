b33e146931c7077e3433f67be921686662ce01c3
hebertcisco <hebertcisco@outlook.com>
2025-02-06 22:44:17 -0300

    Update configuration for valid email access token

--- src/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    /**
-     * The token to use the package. You can get it from http://usevalid.email
-     */
-    'token' => env('USE_VALID_EMAIL_TOKEN'),
-];
--- src/config/usevalidemail.php
+++ src/config/usevalidemail.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'access_token' => env('USE_VALID_EMAIL_TOKEN'),
+    'access_token' => env('USE_VALID_EMAIL_ACCESS_TOKEN'),
     'api_endpoint' => 'https://api.usevalid.email/v1/',
     'timeout' => 10,
 ];
